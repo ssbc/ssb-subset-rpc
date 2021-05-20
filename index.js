ba6707269c2488e5550bdcf84d02e54274c55a94
Anders Rune Jensen <arj03@protonmail.ch>
2021-05-20 16:05:14 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,73 @@
+const { where, toPullStream, descending, and, or, type, author,
+        isPrivate, isPublic } = require('ssb-db2/operators')
+const { reEncrypt } = require('ssb-db2/indexes/private')
+const pull = require('pull-stream')
+
+exports.manifest = {
+  getSubset: 'source',
+}
+
+exports.permissions = {
+  anonymous: { allow: ['getSubset'], deny: null },
+}
+
+exports.init = function (sbot, config) {
+  function formatMsg(msg) {
+    msg = reEncrypt(msg)
+    return msg.value
+  }
+
+  sbot.getSubset = function getSubset(queryObj, opts) {
+    if (!opts) opts = {}
+    
+    const noDataOps = ['isPrivate', 'isPublic']
+    
+    function parse(o) {
+      if (!o.op) throw "missing op"
+      if (!noDataOps.includes(o.op) && !o.data) throw "missing data for " + o.op
+
+      if (o.op == 'and') {
+        if (!Array.isArray(o.data))
+          throw "data part of 'and' op must be an array"
+        
+        let args = o.data.map(op => parse(op))
+        return and(...args)
+      } else if (o.op == 'or') {
+        if (!Array.isArray(o.data))
+          throw "data part of 'and' op must be an array"
+
+        let args = o.data.map(op => parse(op))
+        return or(...args)
+      } else if (o.op == 'type') {
+        if (!typeof(o.data) === 'string')
+          throw "data part of 'type' op must be a string"
+        return type(o.data)
+      } else if (o.op == 'author') {
+        if (!typeof(o.data) === 'string')
+          throw "data part of 'author' op must be a string"
+        return author(o.data)
+      }
+      else if (o.op == 'isPublic')
+        return isPublic()
+      else if (o.op == 'isPrivate')
+        return isPrivate()
+      else
+        throw "Unknown op " + o.op
+    }
+
+    const query = parse(queryObj)
+
+    return pull(
+      sbot.db.query(
+        where(query),
+        opts.descending ? descending() : null,
+        opts.startFrom ? startFrom(opts.startFrom) : null,
+        opts.pageSize ? paginate(opts.pageSize) : null,
+        toPullStream()
+      ),
+      pull.map(msg => formatMsg(msg))
+    )
+  }
+
+  return {}
+}
---
+++ test/query.js
@@ -0,0 +1,123 @@
+const ssbKeys = require('ssb-keys')
+const path = require('path')
+const test = require('tape')
+const pull = require('pull-stream')
+const rimraf = require('rimraf')
+const mkdirp = require('mkdirp')
+const SecretStack = require('secret-stack')
+const caps = require('ssb-caps')
+
+const dir = '/tmp/ssb-getsubset'
+
+rimraf.sync(dir)
+mkdirp.sync(dir)
+
+const keys = ssbKeys.loadOrCreateSync(path.join(dir, 'secret'))
+
+const sbot = SecretStack({ appKey: caps.shs })
+  .use(require('ssb-db2'))
+  .use(require('../'))
+  .call(null, {
+    keys,
+    path: dir,
+  })
+
+test('Base', (t) => {
+  const msg1 = { type: 'post', text: 'a' }
+  const msg2 = { type: 'vote', vote: { value: 1, link: '%abc' } }
+  const msg3 = { type: 'post', text: 'c' }
+  
+  pull(
+    pull.values([msg1, msg2, msg3]),
+    pull.asyncMap((msg, cb) => sbot.db.publish(msg, cb)),
+    pull.collect((err) => {
+      t.error(err)
+      sbot.db.onDrain(() => {
+        pull(
+          sbot.getSubset({ op: 'type', data : 'post' }), // "type('post')"
+          pull.collect((err, results) => {
+            t.error(err)
+            t.equal(results.length, 2, "correct number of results")
+            t.end()
+          })
+        )
+      })
+    })
+  )
+})
+
+test('Advanced', (t) => {
+  pull(
+    sbot.getSubset({
+      op: 'and',
+      data: [
+        { op: 'type', data: 'post' },
+        { op: 'author', data: '@abc' }
+      ]
+    }),
+    pull.collect((err, results) => {
+      t.error(err)
+      t.equal(results.length, 0, "correct number of results")
+
+      pull(
+        sbot.getSubset({
+          op: 'and',
+          data: [
+            { op: 'type', data: 'post' },
+            { op: 'or',
+              data: [
+                { op: 'author', data: '@abc' },
+                { op: 'author', data: sbot.id }
+              ]
+            }
+          ]
+        }),
+        pull.collect((err, results) => {
+          t.error(err)
+          t.equal(results.length, 2, "correct number of results")
+
+          t.end()
+        })
+      )
+    })
+  )
+})
+
+test('public / private', (t) => {
+  let priv = { type: 'post', text: 'super secret', recps: [keys.id] }
+
+  sbot.db.publish(priv, (err) => { 
+    sbot.db.onDrain(() => {
+      pull(
+        sbot.getSubset({
+          op: 'and',
+          data: [
+            { op: 'type', data: 'post' },
+            { op: 'isPublic' }
+          ]
+        }),
+        pull.collect((err, results) => {
+          t.error(err)
+          t.equal(results.length, 2, "correct number of results")
+
+          pull(
+            sbot.getSubset({
+              op: 'and',
+              data: [
+                { op: 'type', data: 'post' },
+                { op: 'isPrivate' }
+              ]
+            }),
+            pull.collect((err, results) => {
+              t.error(err)
+              t.equal(results.length, 1, "correct number of results")
+              t.equal(typeof results[0].content, 'string', "encrypted")
+              
+              sbot.close(t.end)
+            })
+          )
+        })
+      )
+    })
+  })
+})
